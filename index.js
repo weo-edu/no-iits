4354911d5bffbb273baa293a53a54cb72e30516b
Andrew Shaffer <darawk@gmail.com>
2015-06-16 12:23:19 -0700

    handle arguments better; remove semicolons and improve style a bit

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
-var glob = require('glob');
-var fs = require('fs');
-var blacklist = ['it.only(', 'iit(', 'describe.only('];
+var glob = require('glob')
+var fs = require('fs')
+var blacklist = ['it.only(', 'iit(', 'describe.only(']
 
 function read(path) {
   return {
     contents: fs.readFileSync(path, 'utf8'),
     path: path
-  };
+  }
 }
 
 function checkItemFactory(file) {
   return function(item) {
-    var idx = file.contents.indexOf(item);
+    var idx = file.contents.indexOf(item)
     if(idx !== -1) {
-      file.invalid = file.invalid || {};
-      file.invalid[item.slice(0, -1)] = true;
+      file.invalid = file.invalid || {}
+      file.invalid[item.slice(0, -1)] = true
     }
-    return file;
+    return file
   };
 }
 
 function validate(file) {
-  blacklist.forEach(checkItemFactory(file));
-  return file;
+  blacklist.forEach(checkItemFactory(file))
+  return file
+}
+
+function globAll(files, pattern) {
+  return files.concat(glob.sync(pattern))
 }
 
 module.exports = function(patterns) {
-  var files = patterns.reduce(function(memo, pattern) {
-    return memo.concat(glob.sync(pattern));
-  }, []);
-  return files.map(read).map(validate);
+  return patterns
+  .reduce(globAll, [])
+  .map(read)
+  .map(validate)
 };
